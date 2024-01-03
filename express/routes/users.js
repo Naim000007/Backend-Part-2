e2cd4b5a69538788987e6299065043537af0d727
Naim <mdnaim01303202218@gmail.com>
2024-01-03 23:37:52 +0600

    done

--- express/routes/users.js
+++ express/routes/users.js
@@ -1,3 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017")
+mongoose.connect("mongodb://127.0.0.1:27017/practice")
+
+const userschema = mongoose.Schema({
+    username: String,
+    name: String,
+    age:Number
+})
+
+module.exports = mongoose.model(name, Schema)
