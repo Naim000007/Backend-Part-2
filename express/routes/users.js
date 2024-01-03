6198aa80aa8e489d99fcd217166db176a1cd63f5
Naim <mdnaim01303202218@gmail.com>
2024-01-03 23:39:15 +0600

    how to make model step written

--- express/routes/users.js
+++ express/routes/users.js
@@ -1,3 +1,8 @@
+//install mongoosejs
+//require and setup connection 
+// make schema 
+//create model and export 
+
 const mongoose = require('mongoose');
 
 mongoose.connect("mongodb://127.0.0.1:27017/practice")
